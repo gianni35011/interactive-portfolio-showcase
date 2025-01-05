e01db0ad395783e8f91f7573e073514556fdc7e0
Gianni <g35011-accounts@protonmail.com>
2025-01-06 01:41:46 +1100

    GLTF loader added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import './style.css'
 import * as THREE from 'three'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 import Camera from './engine/camera'
 import Light from './engine/light'
 import { Graphics } from './engine/graphics.ts'
+import loader from './tools/loader.ts'
 
 const scene = new THREE.Scene();
 const camera = new Camera();
@@ -15,18 +15,22 @@ const light = new Light();
 
 
 scene.add(light);
-const loader = new GLTFLoader();
-loader.load('src/assets/adventurers/mug_empty.gltf', (gltf) => {
-    gltf.scene.traverse(c => {
-        c.castShadow = true;
-    })
-    scene.add(gltf.scene);
-})
+const meshes = await loader('src/assets/adventurers/mug_empty.gltf')
+scene.add(...meshes.meshes);
+
+// const loader = new GLTFLoader();
+// loader.load('src/assets/adventurers/mug_empty.gltf', (gltf) => {
+//     gltf.scene.traverse(c => {
+//         c.castShadow = true;
+//     })
+//     console.log(gltf.scene)
+//     scene.add(gltf.scene);
+// })
 
 const graphic = new Graphics({scene, camera})
 graphic.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(graphic.domElement);
 
 graphic.onUpdate((dt: number) => {
-    console.log(`Delta time: ${dt}`);
+    // console.log(`Delta time: ${dt}`);
 });
---
+++ src/tools/loader.ts
@@ -0,0 +1,32 @@
+﻿import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import {Object3D} from "three";
+
+const loaderGLTF = new GLTFLoader();
+
+// export default async function loadAsset(path: string): Promise<{ meshes: Object3D[] }> {
+//     const meshes: Object3D[] = [];
+//
+//     const glb = await loaderGLTF.loadAsync(path);
+//
+//     glb.scene.traverse(glb => {
+//         meshes.push(glb);
+//         console.log(glb);
+//     })
+//
+//     return { meshes };
+//
+// }
+
+export default async function loadAsset(path: string): Promise<{ meshes: Object3D[] }> {
+    const meshes: Object3D[] = [];
+
+    const glb = await loaderGLTF.loadAsync(path);
+
+    glb.scene.traverse((child) => {
+        meshes.push(child);
+        console.log(child);
+    })
+
+    return { meshes };
+
+}
