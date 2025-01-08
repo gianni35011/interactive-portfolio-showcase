cf267e03c6e7b5054c1f47b3a71ebebe58cf8cf0
Gianni <g35011-accounts@protonmail.com>
2025-01-09 03:24:00 +1100

    Able to load GLTF and added Rapier physics engine.

---
+++ src/tools/RapierHelper.ts
@@ -0,0 +1,15 @@
+﻿import {ColliderDesc, RigidBodyDesc } from '@dimforge/rapier3d'
+import {Mesh} from "three";
+
+function createColliderGeo(geo, rigidBody, physics){
+    const vertices = new Float32Array(geo.attributes.position.array);
+    const indices = new Uint32Array(geo.index.array);
+    const colliderDesc = ColliderDesc.trimesh(vertices, indices);
+    physics.createCollider(colliderDesc);
+}
+
+export function createRigidBodyFixed(mesh: Mesh, phyicsEngine){
+    const rigidBodyDesc = RigidBodyDesc.fixed()
+    const rigidBody = phyicsEngine.createRigidBody(rigidBodyDesc);
+    createColliderGeo(mesh.geometry, rigidBody, phyicsEngine)
+}
